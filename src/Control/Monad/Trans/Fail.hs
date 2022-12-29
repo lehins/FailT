47a2c7551dc3a4cd511e92a24ce98abf1cfd5455
Alexey Kuleshevich <alexey@kuleshevi.ch>
2022-12-30 00:29:35 +0300

    Stabilizing api

--- src/Control/Monad/Trans/Fail.hs
+++ src/Control/Monad/Trans/Fail.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE LambdaCase #-}
 
 -- |
@@ -9,22 +11,31 @@
 -- Stability   : experimental
 -- Portability : non-portable
 module Control.Monad.Trans.Fail (
-  Fail (..),
+  Fail,
   runFail,
+  runFailLast,
+  runFailAgg,
   errorFail,
   FailT (..),
   runFailT,
+  runFailLastT,
+  runFailAggT,
+  throwFailT,
 ) where
 
 import Control.Applicative
 import Control.Exception
+import Control.Monad
 import qualified Control.Monad.Fail as Fail
 import Control.Monad.Trans
+import Control.Monad.Zip
 import Data.Functor.Identity
-#if !(MIN_VERSION_base(4,9,0))
-import Data.Stack
-#else
+import Data.List (intersperse)
+import qualified Data.List.NonEmpty as NE
+import Data.Semigroup
 import GHC.Stack
+#if MIN_VERSION_base(4,12,0)
+import Data.Functor.Contravariant
 #endif
 
 type Fail = FailT Identity
@@ -33,25 +44,64 @@ runFail :: Fail a -> Either String a
 runFail = runIdentity . runFailT
 {-# INLINE runFail #-}
 
+runFailLast :: Fail a -> Either String a
+runFailLast = runIdentity . runFailLastT
+{-# INLINE runFailLast #-}
+
+runFailAgg :: Fail a -> Either [String] a
+runFailAgg = runIdentity . runFailAggT
+{-# INLINE runFailAgg #-}
+
 errorFail :: HasCallStack => Fail a -> a
 errorFail = either error id . runFail
 
-newtype FailT m a = FailT (m (Either String a))
+newtype FailT m a = FailT (m (Either [String] a))
 
-runFailT :: FailT m a -> m (Either String a)
-runFailT (FailT f) = f
+runFailT :: Functor m => FailT m a -> m (Either String a)
+runFailT (FailT f) = either (Left . sconcat . NE.intersperse "\n" . toFailureNonEmpty) Right <$> f
 {-# INLINE runFailT #-}
+
+runFailLastT :: Functor m => FailT m a -> m (Either String a)
+runFailLastT (FailT f) = either (Left . NE.head . toFailureNonEmpty) Right <$> f
+{-# INLINE runFailLastT #-}
+
+runFailAggT :: FailT m a -> m (Either [String] a)
+runFailAggT (FailT f) = f
+{-# INLINE runFailAggT #-}
+
 
 data FailException = FailException
   { failCallStack :: CallStack
-  , failMessage :: String
+  , failMessages :: [String]
   }
-  deriving (Show)
+
+instance Show FailException where
+  show FailException{failCallStack, failMessages} =
+    mconcat $
+      intersperse "\n" $
+        "FailException"
+          : NE.toList (toFailureNonEmpty failMessages)
+          ++ [prettyCallStack failCallStack]
 
 instance Exception FailException
 
--- instance MonadTrans m => MonadTrans (FailT m) where
---   lift f
+toFailureNonEmpty :: [String] -> NE.NonEmpty String
+toFailureNonEmpty xs =
+  case NE.nonEmpty xs of
+    Nothing -> "No failure reason given" NE.:| []
+    Just ne -> ne
+
+throwFailT :: (HasCallStack, MonadIO m) => FailT m a -> m a
+throwFailT f = do
+  runFailAggT f >>= \case
+    Right x -> pure x
+    Left errMsgs ->
+      liftIO $
+        throwIO $
+          FailException
+            { failCallStack = ?callStack
+            , failMessages = errMsgs
+            }
 
 instance Functor m => Functor (FailT m) where
   fmap f (FailT m) = FailT (fmap (fmap f) m)
@@ -76,13 +126,8 @@ instance Monad m => Monad (FailT m) where
     FailT $
       m >>= \case
         Left merr -> return $ Left merr
-        Right a -> runFailT $ k a
+        Right a -> runFailAggT $ k a
   {-# INLINE (>>=) #-}
-
-#if !(MIN_VERSION_base(4,8,0))
-  return a = FailT $ return $ Right a
-  {-# INLINE return #-}
-#endif
 
 #if !(MIN_VERSION_base(4,13,0))
   fail = FailT . return . Left
@@ -90,7 +135,7 @@ instance Monad m => Monad (FailT m) where
 #endif
 
 instance Monad m => Fail.MonadFail (FailT m) where
-  fail = FailT . return . Left
+  fail = FailT . return . Left . pure
   {-# INLINE fail #-}
 
 instance Foldable f => Foldable (FailT f) where
@@ -101,14 +146,61 @@ instance Traversable f => Traversable (FailT f) where
   traverse f (FailT m) = FailT <$> traverse (either (pure . Left) (fmap Right . f)) m
   {-# INLINE traverse #-}
 
+plus :: Monad m => FailT m a -> FailT m a -> FailT m a
+plus (FailT m) (FailT k) = FailT $ do
+  m >>= \case
+    Left merr ->
+      k >>= \case
+        Left kerr -> pure $ Left $ merr ++ kerr
+        Right result -> pure $ Right $ result
+    Right x -> pure (Right x)
+{-# INLINEABLE plus #-}
+
 instance Monad m => Alternative (FailT m) where
-  empty = FailT $ pure (Left "")
+  empty = FailT $ pure (Left [])
   {-# INLINE empty #-}
-  FailT m <|> FailT k = FailT $ do
-    m >>= \case
+  (<|>) = plus
+  {-# INLINE (<|>) #-}
+
+instance Monad m => MonadPlus (FailT m) where
+  mzero = empty
+  {-# INLINE mzero #-}
+  mplus = plus
+  {-# INLINE mplus #-}
+
+instance (Monad m, Semigroup a) => Semigroup (FailT m a) where
+  (<>) (FailT m) (FailT k) = FailT $ do
+    mres <- m
+    kres <- k
+    case mres of
       Left merr ->
-        k >>= \case
+        case kres of
           Left kerr -> pure $ Left $ merr ++ kerr
-          Right result -> pure $ Right $ result
-      Right x -> pure (Right x)
-  {-# INLINEABLE (<|>) #-}
+          Right y -> pure $ Right y
+      Right x ->
+        case kres of
+          Left _kerr -> pure $ Right x
+          Right y -> pure $ Right (x <> y)
+  {-# INLINEABLE (<>) #-}
+
+instance (Monad m, Semigroup a) => Monoid (FailT m a) where
+  mempty = empty
+  {-# INLINE mempty #-}
+
+instance MonadIO m => MonadIO (FailT m) where
+  liftIO = lift . liftIO
+  {-# INLINE liftIO #-}
+
+instance MonadTrans FailT where
+  lift = FailT . fmap Right
+  {-# INLINE lift #-}
+
+instance MonadZip m => MonadZip (FailT m) where
+  mzipWith f (FailT a) (FailT b) = FailT $ mzipWith (liftA2 f) a b
+  {-# INLINE mzipWith #-}
+
+#if MIN_VERSION_base(4,12,0)
+instance Contravariant f => Contravariant (FailT f) where
+  contramap f = FailT . contramap (fmap f) . runFailAggT
+  {-# INLINE contramap #-}
+#endif
